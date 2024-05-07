8f8213db52779cc242121eb3e3d129775d657c4f
najeebib <najeebibraheem1999@hotmail.com>
2024-05-07 10:10:07 +0300

    added some comments

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,11 @@
 def fibonacci_recursive(n):
+    # closing condition
     if n <= 1:
         return n
     else:
         return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 
-
+# added comments
 def fibonacci_dynamic(n):
     fib = [0, 1]
     for i in range(2, n + 1):
